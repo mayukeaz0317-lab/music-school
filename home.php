4f8097c3a09b6c50cfb71a5d0d9758da3074fdcb
平松慶太 <hiramatsukeita@hiramatsukeitanoMacBook-Air.local>
2026-06-14 10:56:40 +0900

    refactor:esc処理追加、パンくずリスト構造化

--- home.php
+++ home.php
@@ -6,10 +6,21 @@
     <nav class="breadcrumbs" aria-label="パンくずリスト">
         <div class="container">
             <div class="breadcrumbs__inner">
-                <ul class="breadcrumbs__list">
-                    <li class="breadcrumbs__item"><a href="<?php echo esc_url(home_url('/top/')); ?>">ホーム</a></li>
-                    <li class="breadcrumbs__item" aria-current="page">ブログ</li>
-                </ul>
+                <ol class="breadcrumbs__list" itemscope itemtype="https://schema.org/BreadcrumbList">
+                    <?php $position = 1; ?>
+
+                    <li class="breadcrumbs__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                        <a itemprop="item" href="<?php echo esc_url(home_url('/')); ?>">
+                            <span itemprop="name">ホーム</span>
+                        </a>
+                        <meta itemprop="position" content="<?php echo $position++; ?>" />
+                    </li>
+
+                    <li class="breadcrumbs__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" aria-current="page">
+                        <span itemprop="name">ブログ</span>
+                        <meta itemprop="position" content="<?php echo $position++; ?>" />
+                    </li>
+                </ol>
             </div>
         </div>
     </nav>
@@ -19,22 +30,22 @@
             <ul>
                 <?php if (have_posts()): while (have_posts()): the_post(); ?>
                         <li class="blog-list__item">
-                            <a href="<?php the_permalink(); ?>">
+                            <a href="<?php echo esc_url(get_the_permalink()) ?>">
                                 <div class="blog-list__img">
                                     <?php if (has_post_thumbnail()): ?>
-                                        <?php the_post_thumbnail('full') ?>
+                                        <?php the_post_thumbnail('large') ?>
                                     <?php else : ?>
-                                        <img src="<?php echo get_theme_file_uri('/images/photo/no-image.jpg'); ?>" alt="">
+                                        <img src="<?php echo esc_url(get_theme_file_uri('/images/photo/no-image.jpg')); ?>" alt="">
                                     <?php endif; ?>
                                     <span class="img__heading">
                                         <?php $cat = get_the_category();
-                                        echo $cat[0]->name; ?>
+                                        if (!empty($cat)) echo esc_html($cat[0]->name); ?>
                                     </span>
                                 </div>
                                 <div class="blog-list__text">
-                                    <h3 class="blog-item__text"><?php the_title(); ?></h3>
+                                    <h3 class="blog-item__text"><?php echo esc_html(get_the_title()); ?></h3>
                                     <time class="blog-item__time" datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
-                                    <p><?php echo get_the_excerpt(); ?></p>
+                                    <p><?php the_excerpt(); ?></p>
                                 </div>
                             </a>
                         </li>
@@ -45,7 +56,7 @@
     </section>
     <div class="back-to-top">
         <a href="#header">
-            <img src="<?php echo get_theme_file_uri('/images/icons/back-to-top.svg') ?>" alt="ページトップへ戻る">
+            <img src="<?php echo esc_url(get_theme_file_uri('/images/icons/back-to-top.svg')) ?>" alt="ページトップへ戻る">
         </a>
     </div>
     <div class="contact-cta btn">
