b16f5d08a3ef48875adcbdf43608ca2aa4d4392a
平松慶太 <hiramatsukeita@hiramatsukeitanoMacBook-Air.local>
2026-06-14 15:51:14 +0900

    refactor: alt付与、esc処理追加

--- front-page.php
+++ front-page.php
@@ -8,8 +8,8 @@
             <h2 class="concept__title">全人類、<br class="sp-only">ミュージシャン計画。</h2>
             <p class="concept__desc">私たちは音楽を愛するすべての人が、音楽に熱狂できる世界を目指しています。</p>
             <picture class="concept__semicircle">
-                <source srcset="<?php echo get_theme_file_uri('/images/icons/semicircle-sp.svg') ?>" media="(max-width: 576px)">
-                <img src="<?php echo get_theme_file_uri('/images/icons/semicircle.svg') ?>" alt="">
+                <source srcset="<?php echo esc_url(get_theme_file_uri('/images/icons/semicircle-sp.svg')) ?>" media="(max-width: 576px)">
+                <img src="<?php echo esc_url(get_theme_file_uri('/images/icons/semicircle.svg')) ?>" alt="">
             </picture>
             <dl class="concept-flow">
                 <div class="concept-flow__item">
@@ -39,8 +39,8 @@
             <ul class="reasons__list">
                 <li class="reasons__item">
                     <picture class="reasons__img">
-                        <source srcset="<?php echo get_theme_file_uri('/images/photo/reason01-sp.jpg') ?>" media="(max-width:768px)">
-                        <img src="<?php echo get_theme_file_uri('/images/photo/reason01.jpg') ?>" alt="">
+                        <source srcset="<?php echo esc_url(get_theme_file_uri('/images/photo/reason01-sp.jpg')) ?>" media="(max-width:768px)">
+                        <img src="<?php echo esc_url(get_theme_file_uri('/images/photo/reason01.jpg')) ?>" alt="プロの講師による丁寧なピアノのマンツーマンレッスンの様子">
                     </picture>
                     <div class="reasons__text">
                         <h3 class="reasons__title">技術面はプロによるマンツーマン授業！</h3>
@@ -49,8 +49,8 @@
                 </li>
                 <li class="reasons__item">
                     <picture class="reasons__img">
-                        <source srcset="<?php echo get_theme_file_uri('/images/photo/reason02-sp.jpg') ?>" media="(max-width:768px)">
-                        <img src="<?php echo get_theme_file_uri('/images/photo/reason02.jpg') ?>" alt="">
+                        <source srcset="<?php echo esc_url(get_theme_file_uri('/images/photo/reason02-sp.jpg')) ?>" media="(max-width:768px)">
+                        <img src="<?php echo esc_url(get_theme_file_uri('/images/photo/reason02.jpg')) ?>" alt="音楽ビジネスや集客方法についてスタッフと笑顔で個別面談・相談をする様子">
                     </picture>
                     <div class="reasons__text">
                         <h3 class="reasons__title">収益化するためのビジネスサポート！</h3>
@@ -60,8 +60,8 @@
                 </li>
                 <li class="reasons__item">
                     <picture class="reasons__img">
-                        <source srcset="<?php echo get_theme_file_uri('/images/photo/reason03-sp.jpg') ?>" media="(max-width:768px)">
-                        <img src="<?php echo get_theme_file_uri('/images/photo/reason03.jpg') ?>" alt="">
+                        <source srcset="<?php echo esc_url(get_theme_file_uri('/images/photo/reason03-sp.jpg')) ?>" media="(max-width:768px)">
+                        <img src="<?php echo esc_url(get_theme_file_uri('/images/photo/reason03.jpg')) ?>" alt="個室の練習ROOMでアコースティックギターを弾きながら楽譜に書き込みをする様子">
                     </picture>
                     <div class="reasons__text">
                         <h3 class="reasons__title">24時間365日使える練習ROOMを完備！</h3>
@@ -78,13 +78,13 @@
         <div class="container">
             <h2 class="index__title index__title--testimonial">生徒さんたちの声</h2>
             <div class="testimonial__slider">
-                <button class="slider-btn prev"><img src="<?php echo get_theme_file_uri('/images/icons/arrow-l.png') ?>" alt=""></button>
-                <button class="slider-btn next"><img src="<?php echo get_theme_file_uri('/images/icons/arrow-r.png') ?>" alt=""></button>
+                <button class="slider-btn prev" aria-label="前へ"><img src="<?php echo esc_url(get_theme_file_uri('/images/icons/arrow-l.png')) ?>" alt=""></button>
+                <button class="slider-btn next" aria-label="次へ"><img src="<?php echo esc_url(get_theme_file_uri('/images/icons/arrow-r.png')) ?>" alt=""></button>
                 <div class="testimonial__viewport">
                     <ul class="testimonial__list">
                         <li class="testimonial__item">
                             <div class="testimonial__img">
-                                <img src="<?php echo get_theme_file_uri('/images/photo/voice01.jpg') ?>" alt="">
+                                <img src="<?php echo esc_url(get_theme_file_uri('/images/photo/voice01.jpg')) ?>" alt="笑顔でマイクを持って楽しそうに歌う男性の姿">
                             </div>
                             <div class="testimonial__text">
                                 <h3 class="testimonial__title">証券会社勤務　丸山さん</h3>
@@ -93,7 +93,7 @@
                         </li>
                         <li class="testimonial__item">
                             <div class="testimonial__img">
-                                <img src="<?php echo get_theme_file_uri('/images/photo/voice02.jpg') ?>" alt="">
+                                <img src="<?php echo esc_url(get_theme_file_uri('/images/photo/voice02.jpg')) ?>" alt="ピアノの前に座り可愛い小型犬を抱きしめて微笑む女性の姿">
                             </div>
                             <div class="testimonial__text">
                                 <h3 class="testimonial__title">IT会社勤務　S.Eさん</h3>
@@ -102,7 +102,7 @@
                         </li>
                         <li class="testimonial__item">
                             <div class="testimonial__img">
-                                <img src="<?php echo get_theme_file_uri('/images/photo/voice03.jpg') ?>" alt="">
+                                <img src="<?php echo esc_url(get_theme_file_uri('/images/photo/voice03.jpg')) ?>" alt="アコースティックギターを抱えて真剣な表情で手元を見つめる男性の姿">
                             </div>
                             <div class="testimonial__text">
                                 <h3 class="testimonial__title">IT会社勤務　S.Eさん</h3>
@@ -111,7 +111,7 @@
                         </li>
                         <li class="testimonial__item">
                             <div class="testimonial__img">
-                                <img src="<?php echo get_theme_file_uri('/images/photo/voice02.jpg') ?>" alt="">
+                                <img src="<?php echo esc_url(get_theme_file_uri('/images/photo/voice02.jpg')) ?>" alt="ピアノの前に座り可愛い小型犬を抱きしめて微笑む女性の姿">
                             </div>
                             <div class="testimonial__text">
                                 <h3 class="testimonial__title">IT会社勤務　S.Eさん</h3>
@@ -120,7 +120,7 @@
                         </li>
                         <li class="testimonial__item">
                             <div class="testimonial__img">
-                                <img src="<?php echo get_theme_file_uri('/images/photo/voice03.jpg') ?>" alt="">
+                                <img src="<?php echo esc_url(get_theme_file_uri('/images/photo/voice03.jpg')) ?>" alt="アコースティックギターを抱えて真剣な表情で手元を見つめる男性の姿">
                             </div>
                             <div class="testimonial__text">
                                 <h3 class="testimonial__title">IT会社勤務　S.Eさん</h3>
@@ -238,9 +238,9 @@
                             <a href="<?php the_permalink(); ?>">
                                 <div class="index-blog__img">
                                     <?php if (has_post_thumbnail()) : ?>
-                                        <?php the_post_thumbnail('full'); ?>
+                                        <?php the_post_thumbnail('medium'); ?>
                                     <?php else : ?>
-                                        <img src="<?php echo get_theme_file_uri('/images/photo/no-image.jpg'); ?>" alt="No Image">
+                                        <img src="<?php echo esc_url(get_theme_file_uri('/images/photo/no-image.jpg')); ?>" alt="No Image">
                                     <?php endif; ?>
                                     <span class="img__heading">
                                         <?php
@@ -253,7 +253,7 @@
                                         ?>
                                     </span>
                                 </div>
-                                <h3 class="index-blog__title"><?php the_title(); ?></h3>
+                                <h3 class="index-blog__title"><?php esc_html(get_the_title()); ?></h3>
                                 <time class="index-blog__time" datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d'); ?></time>
                             </a>
                         </li>
