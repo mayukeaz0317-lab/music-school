1cb3106e758b43da65c7b4a8db0386ad2793457b
平松慶太 <hiramatsukeita@hiramatsukeitanoMacBook-Air.local>
2026-06-14 12:39:38 +0900

    refactor: esc処理追加とパンくずリスト構造化

--- archive.php
+++ archive.php
@@ -6,38 +6,49 @@
     <nav class="breadcrumbs" aria-label="パンくずリスト">
         <div class="container">
             <div class="breadcrumbs__inner">
-                <ul class="breadcrumbs__list">
-                    <li class="breadcrumbs__item"><a href="<?php echo esc_url(home_url('/top/')); ?>">ホーム</a></li>
-                    <li class="breadcrumbs__item" aria-current="page"><?php the_archive_title(); ?></li>
-                </ul>
+                <ol class="breadcrumbs__list" itemscope itemtype="https://schema.org/BreadcrumbList">
+                    <?php $position = 1; ?>
+
+                    <li class="breadcrumbs__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                        <a itemprop="item" href="<?php echo esc_url(home_url('/')); ?>">
+                            <span itemprop="name">ホーム</span>
+                        </a>
+                        <meta itemprop="position" content="<?php echo $position++; ?>" />
+                    </li>
+
+                    <li class="breadcrumbs__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" aria-current="page">
+                        <span itemprop="name"><?php echo esc_html(single_term_title('', false) ?: post_type_archive_title('', false)); ?></span>
+                        <meta itemprop="position" content="<?php echo $position++; ?>" />
+                    </li>
+                </ol>
             </div>
         </div>
     </nav>
     <section class="blog-list__content">
         <div class="container">
-            <h2 class="section-title section-title--blog-list"><?php the_archive_title(); ?></h2>
+            <h2 class="section-title section-title--blog-list"><?php esc_html(get_the_archive_title()); ?></h2>
             <ul>
                 <?php if (have_posts()): while (have_posts()): the_post(); ?>
                         <li class="blog-list__item">
-                            <a href="<?php the_permalink(); ?>">
+                            <a href="<?php echo esc_url(get_the_permalink()); ?>">
                                 <div class="blog-list__img">
                                     <?php if (has_post_thumbnail()): ?>
-                                        <?php the_post_thumbnail('full') ?>
+                                        <?php the_post_thumbnail('large') ?>
                                     <?php else : ?>
-                                        <img src="<?php echo get_theme_file_uri('/images/photo/no-image.jpg'); ?>" alt="">
+                                        <img src="<?php echo esc_url(get_theme_file_uri('/images/photo/no-image.jpg')); ?>" alt="">
                                     <?php endif; ?>
                                     <span class="img__heading">
                                         <?php if (get_post_type() === 'result') {
 
                                             $terms = get_the_terms(get_the_ID(), 'result_genre');
-                                            if (!empty($terms)&& !is_wp_error($terms)) {
-                                                echo $terms[0]->name;
+                                            if (!empty($terms) && !is_wp_error($terms)) {
+                                                echo esc_html($terms[0]->name);
                                             }
                                         } else {
 
                                             $cat = get_the_category();
                                             if (!empty($cat)) {
-                                                echo $cat[0]->name;
+                                                echo esc_html($cat[0]->name);
                                             }
                                         } ?>
                                     </span>
@@ -45,7 +56,7 @@
                                 <div class="blog-list__text">
                                     <h3 class="blog-item__text"><?php the_title(); ?></h3>
                                     <time class="blog-item__time" datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
-                                    <p><?php the_excerpt(); ?></p>
+                                    <?php the_excerpt(); ?>
                                 </div>
                             </a>
                         </li>
@@ -56,7 +67,7 @@
     </section>
     <div class="back-to-top">
         <a href="#header">
-            <img src="<?php echo esc_url(get_theme_file_uri('/images/icons/back-to-top.svg') )?>" alt="ページトップへ戻る">
+            <img src="<?php echo esc_url(get_theme_file_uri('/images/icons/back-to-top.svg')) ?>" alt="ページトップへ戻る">
         </a>
     </div>
     <div class="contact-cta btn">
